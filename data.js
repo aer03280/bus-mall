baf4a7669bc973d286793d14916956a81703804c
Ashley Ross <ashley.e.ross@gmail.com>
2016-11-01 22:44:49 -0700

    chart framework added

--- data.js
+++ data.js
@@ -1,76 +1,45 @@
 'use strict';
 
-// var arrayAllImg = [
-//   new Image('bag' , 'assets/bag.jpg'),
-//   new Image('bathroom' , 'assets/bathroom.jpg'),
-//   new Image('boots' , 'assets/boots.jpg'),
-//   new Image('breakfast' , 'assets/breakfast.jpg'),
-//   new Image('bubblegum' , 'assets/bubblegum.jpg'),
-//   new Image('chair' , 'assets/chair.jpg'),
-//   new Image('cthulhu' , 'assets/cthulhu.jpg'),
-//   new Image('dog duck' , 'assets/dog-duck.jpg'),
-//   new Image('dragon' , 'assets/dragon.jpg'),
-//   new Image('pen' , 'assets/pen.jpg'),
-//   new Image('pet sweep' , 'assets/pet-sweep.jpg'),
-//   new Image('scissors' , 'assets/scissors.jpg'),
-//   new Image('shark' , 'assets/shark.jpg'),
-//   new Image('sweep' , 'assets/sweep.jpg'),
-//   new Image('tauntaun' , 'assets/tauntaun.jpg'),
-//   new Image('unicorn' , 'assets/unicorn.jpg'),
-//   new Image('usb' , 'assets/usb.jpg'),
-//   new Image('water can' , 'assets/water-can.jpg'),
-//   new Image('wine glass' , 'assets/wine-glass.jpg'),
-// ];
-// var arrayAllImg = [];
-//
-// // Constructor to make an array of all image objects
-// function Image (imgName , filePath){
-//   this.imgName = imgName;
-//   this.filePath = filePath;
-//   this.shown = 0;
-//   this.clicked = 0;
-//   arrayAllImg.push(this);
-// };
-//
-// // place function here to run the below (eventhandler)
-// function loadImage(){
-//
-//   // function to generate a random number from the length of the array containing all images
-//   function randomNumbGenerator(){
-//     var randomNumb = parseInt(Math.random() * arrayAllImg.length);
-//     return randomNumb;
-//   };
-//
-//   // provides randomly generated number from randomNumbGenerator function
-//   var randomGenOne = randomNumbGenerator();
-//   var randomGenTwo = randomNumbGenerator();
-//   var randomGenThree = randomNumbGenerator();
-//
-//   // places randomly generated number into arrayAllImg to pick img from the array
-//   var displayImgOne = arrayAllImg[randomGenOne];
-//   var displayImgTwo = arrayAllImg[randomGenTwo];
-//   var displayImgThree = arrayAllImg[randomGenThree];
-//
-//   // get each img by it's id
-//   var imgOne = document.getElementbyId('imgOne');
-//   var imgTwo = document.getElementbyId('imgTwo');
-//   var imgThree = document.getElementbyId('imgThree');
-//
-//   // telling interpretor to look for filepath to display
-//   imgOne.setAttribute('src' , imgOne.filePath);
-//   imgTwo.setAttribute('src' , imgTwo.filePath);
-//   imgThree.setAttribute('src' , imgThree.filePath);
-//
-// }
-//
-// var arrayAllImages = [
-//   new Image ('bag' , 'assets/bag.jpg'),
-//   new Image ('banana' , 'assets/banana.jpg'),
-//   new Image ('bathroom' , 'assets/bathroom.jpg'),
-//   new Image ('boots' , 'assets/boots.jpg'),
-//   new Image ('breakfast' , 'assets/breakfast.jpg'),
-// ];
-//
-// imgOne.addEventListener('click' , loadImage);
-// imgTwo.addEventListener('click' , loadImage);
-// imgThree.addEventListener('click' , loadImage);
+new Chart (ctx, {
+  type: 'bar',
+  data: {
+    labels: ['bag' , 'banana' , 'bathroom' , 'boots' , 'breakfast' , 'bubblegum' ,'chair' , 'cthulhu' , 'dog duck' , 'dragon' , 'pen' , 'pet sweep' , 'scissors' , 'shark' , 'sweep' , 'tauntaun' , 'unicorn' , 'usb' , 'water can' , 'wine glass'
+  ]
+
+  datasets: [{
+  // label: 'Survey Results',
+      data: [totalClicks[i]],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1,
+    }]
+
+  },
+
+  options: {
+    scales: {
+          // xAxes: [{
+              // stacked: true
+          // }],
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});

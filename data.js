15e115919415533a94ffb7162fe6a9cf7f1746e9
Ashley Ross <ashley.e.ross@gmail.com>
2016-11-03 13:48:29 -0700

    adding css changes

--- data.js
+++ data.js
@@ -1,74 +1,55 @@
 'use strict';
 
-// var dataToDisplay = {
-//   labels:[arrayAllImg['imgName']],
-//   datasets: [
-//     {
-//       label: 'Number of Clicks for each Item',
-//       backgroundColor: [
-//         'rgba(255, 99, 132, 0.2)',
-//         'rgba(54, 162, 235, 0.2)',
-//         'rgba(255, 206, 86, 0.2)',
-//         'rgba(75, 192, 192, 0.2)',
-//         'rgba(153, 102, 255, 0.2)',
-//         'rgba(255, 159, 64, 0.2)'
-//       ],
-//       borderColor: [
-//         'rgba(255,99,132,1)',
-//         'rgba(54, 162, 235, 1)',
-//         'rgba(255, 206, 86, 1)',
-//         'rgba(75, 192, 192, 1)',
-//         'rgba(153, 102, 255, 1)',
-//         'rgba(255, 159, 64, 1)'
-//       ],
-//       borderWidth: 1,
-//       data: [arrayAllImg[i].clicks],
-//
-//     }
-//
-//   ]
-// };
-var myChart = new Chart (ctx);
-new Chart (ctx, {
-  type: 'bar',
-  data: {
-    labels:['Image Name'],
-    datasets: [
-      {
-        label: 'Number of Clicks for each Item',
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255,99,132,1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1,
-        data: [arrayAllImg[i].clicks],
-
-      }
-
-    ]
-  },
-  options: {
-    scales: {
-      xAxes: [{
-        stacked: true
-      }],
-      yAxes: [{
-        stacked: true
-      }]
-    }
+function drawChart() {
+  var dataToChart = [];
+  var namesToChart = [];
+  for (var i = 0; i < arrayAllImg; i++){
+    dataToChart.push(arrayAllImg[i].clicks);
+    namesToChart.push(arrayAllImg[i].imgName);
   }
-});
 
-//     labels: ['bag' , 'banana' , 'bathroom' , 'boots' , 'breakfast' , 'bubblegum' ,'chair' , 'cthulhu' , 'dog duck' , 'dragon' , 'pen' , 'pet sweep' , 'scissors' , 'shark' , 'sweep' , 'tauntaun' , 'unicorn' , 'usb' , 'water can' , 'wine glass'
+  var myChart = new Chart (ctx);
+  new Chart (ctx, {
+    type: 'bar',
+    data: {
+      labels:['Image Name'],
+      datasets: [
+        {
+          label: 'Number of Clicks for each Item',
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1,
+          data: [arrayAllImg[i].clicks],
+
+        }
+
+      ]
+    },
+    options: {
+      scales: {
+        xAxes: [{
+          stacked: true
+        }],
+        yAxes: [{
+          stacked: true
+        }]
+      }
+    }
+  });
+
+}
+  //     labels: ['bag' , 'banana' , 'bathroom' , 'boots' , 'breakfast' , 'bubblegum' ,'chair' , 'cthulhu' , 'dog duck' , 'dragon' , 'pen' , 'pet sweep' , 'scissors' , 'shark' , 'sweep' , 'tauntaun' , 'unicorn' , 'usb' , 'water can' , 'wine glass'
